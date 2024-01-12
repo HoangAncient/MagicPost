c3ae4096f986c4c740518fb72e51fca52a06a24f
Hoang Ancient <hoangdinhnho23@gmail.com>
2024-01-13 02:24:23 +0700

    Hello World

--- src/pages/axios/Instance.js
+++ src/pages/axios/Instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "https://magicpostbe.onrender.com/", // Replace with your API base URL
+  baseURL: "http://103.176.147.108:5500/", // Replace with your API base URL
   withCredentials: true // Enable sending cookies with requests
 });
 
